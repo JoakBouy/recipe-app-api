6808500f270e079c795d6e5172f13cd6368260cc
JoakBouy <B.joak@alustudent.com>
2023-03-31 17:52:20 +0300

    Updated setting to imlement user app

--- app/app/settings.py
+++ app/app/settings.py
@@ -40,9 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
     'rest_framework',
     'drf_spectacular',
-    'user',
+
 ]
 
 MIDDLEWARE = [
