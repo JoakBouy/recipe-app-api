3a92d448c65e07109696f84fa4bb246ccf0506ed
JoakBouy <B.joak@alustudent.com>
2023-04-12 12:39:31 +0300

    update

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -1,5 +1,6 @@
 """Tests for the user API"""
 
+from typing import Self
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
@@ -90,7 +91,6 @@ def create_user(**params):
         """Test posting a blank password returns error"""
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
-
-       self.assertNotIn('token', res.data)
-       self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
